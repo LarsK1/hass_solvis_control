65a1439d02c527082e71330a09f5db77a889a8c5
Lars Kusch <lars@lars-kusch.de>
2024-10-07 16:44:37 +0200

    Bugfix

--- custom_components/solvis_control/coordinator.py
+++ custom_components/solvis_control/coordinator.py
@@ -89,10 +89,10 @@ class SolvisModbusCoordinator(DataUpdateCoordinator):
                     parsed_data[register.name] = (
                         abs(value) if register.absolute_value else value
                     )
-                    if "version" in register.name:
-                        parsed_data[register.name] = ".".join(
-                            parsed_data[register.name].split(0, 2)
-                        )
+                    # if "version" in register.name:
+                    #    parsed_data[register.name] = ".".join(
+                    #        parsed_data[register.name].split(0, 2)
+                    #    )
 
                 except ModbusException as error:
                     _LOGGER.error(
