a86cf619e0f666883c6f04e55368bdeb5d082502
Lars Kusch <lars@lars-kusch.de>
2024-11-29 14:32:35 +0100

    Fixes

--- custom_components/solvis_control/config_flow.py
+++ custom_components/solvis_control/config_flow.py
@@ -75,15 +75,9 @@ def get_solvis_modules(data: ConfigType) -> Schema:
 def get_solvis_devices(data: ConfigType) -> Schema:
     return vol.Schema(
         {
-            vol.Required(
-                DEVICE_VERSION, default=str(SolvisDeviceVersion.SC3)
-            ): SolvisVersionSelect,
-            vol.Required(POLL_RATE_DEFAULT, default=30): vol.All(
-                vol.Coerce(int), vol.Range(min=30)
-            ),
-            vol.Required(POLL_RATE_SLOW, default=300): vol.All(
-                vol.Coerce(int), vol.Range(min=60)
-            ),
+            vol.Required(DEVICE_VERSION, default=str(SolvisDeviceVersion.SC3)): SolvisVersionSelect,
+            vol.Required(POLL_RATE_DEFAULT, default=30): vol.All(vol.Coerce(int), vol.Range(min=30)),
+            vol.Required(POLL_RATE_SLOW, default=300): vol.All(vol.Coerce(int), vol.Range(min=60)),
         },
         extra=vol.ALLOW_EXTRA,
     )
@@ -92,18 +86,10 @@ def get_solvis_devices(data: ConfigType) -> Schema:
 def get_solvis_modules_options(data: ConfigType) -> Schema:
     return vol.Schema(
         {
-            vol.Required(
-                CONF_OPTION_1, default=data.get(CONF_OPTION_1, False)
-            ): bool,  # HKR 2
-            vol.Required(
-                CONF_OPTION_2, default=data.get(CONF_OPTION_2, False)
-            ): bool,  # HKR 3
-            vol.Required(
-                CONF_OPTION_3, default=data.get(CONF_OPTION_3, False)
-            ): bool,  # solar collectors
-            vol.Required(
-                CONF_OPTION_4, default=data.get(CONF_OPTION_4, False)
-            ): bool,  # heat pump
+            vol.Required(CONF_OPTION_1, default=data.get(CONF_OPTION_1, False)): bool,  # HKR 2
+            vol.Required(CONF_OPTION_2, default=data.get(CONF_OPTION_2, False)): bool,  # HKR 3
+            vol.Required(CONF_OPTION_3, default=data.get(CONF_OPTION_3, False)): bool,  # solar collectors
+            vol.Required(CONF_OPTION_4, default=data.get(CONF_OPTION_4, False)): bool,  # heat pump
         }
     )
 
@@ -111,15 +97,9 @@ def get_solvis_modules_options(data: ConfigType) -> Schema:
 def get_solvis_devices_options(data: ConfigType) -> Schema:
     return vol.Schema(
         {
-            vol.Required(
-                DEVICE_VERSION, default=str(SolvisDeviceVersion.SC3)
-            ): SolvisVersionSelect,
-            vol.Required(POLL_RATE_DEFAULT, default=30): vol.All(
-                vol.Coerce(int), vol.Range(min=30)
-            ),
-            vol.Required(POLL_RATE_SLOW, default=300): vol.All(
-                vol.Coerce(int), vol.Range(min=60)
-            ),
+            vol.Required(DEVICE_VERSION, default=str(SolvisDeviceVersion.SC3)): SolvisVersionSelect,
+            vol.Required(POLL_RATE_DEFAULT, default=30): vol.All(vol.Coerce(int), vol.Range(min=30)),
+            vol.Required(POLL_RATE_SLOW, default=300): vol.All(vol.Coerce(int), vol.Range(min=60)),
         },
         extra=vol.ALLOW_EXTRA,
     )
@@ -152,19 +132,15 @@ class SolvisConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             self._abort_if_unique_id_configured()
             return await self.async_step_device()
 
-        return self.async_show_form(
-            step_id="user", data_schema=get_host_schema_config(self.data), errors=errors
-        )
+        return self.async_show_form(step_id="user", data_schema=get_host_schema_config(self.data), errors=errors)
 
-    async def async_step_device(
-        self, user_input: ConfigType | None = None
-    ) -> FlowResult:
+    async def async_step_device(self, user_input: ConfigType | None = None) -> FlowResult:
         """Handle the device step."""
         errors = {}
         if user_input is not None:
             try:
                 self.data.update(user_input)
-                await validate_poll_rates(self.data)
+                validate_poll_rates(self.data)
                 return await self.async_step_features()
             except vol.Invalid as exc:
                 errors["base"] = str(exc)
@@ -175,14 +151,10 @@ class SolvisConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             errors=errors,
         )
 
-    async def async_step_features(
-        self, user_input: ConfigType | None = None
-    ) -> FlowResult:
+    async def async_step_features(self, user_input: ConfigType | None = None) -> FlowResult:
         """Handle the feature step."""
         if user_input is None:
-            return self.async_show_form(
-                step_id="features", data_schema=get_solvis_modules(self.data)
-            )
+            return self.async_show_form(step_id="features", data_schema=get_solvis_modules(self.data))
         self.data.update(user_input)
         return self.async_create_entry(title=self.data[CONF_NAME], data=self.data)
 
@@ -219,16 +191,14 @@ class SolvisOptionsFlow(config_entries.OptionsFlow):
             errors=errors,
         )
 
-    async def async_step_device(
-        self, user_input: ConfigType | None = None
-    ) -> FlowResult:
+    async def async_step_device(self, user_input: ConfigType | None = None) -> FlowResult:
         """Handle the device step."""
         errors = {}
         _LOGGER.debug(f"Options flow values_1: {str(self.data)}", DOMAIN)
         if user_input is not None:
             try:
                 self.data.update(user_input)
-                await validate_poll_rates(self.data)
+                validate_poll_rates(self.data)
                 return await self.async_step_features()
             except vol.Invalid as exc:
                 errors["base"] = str(exc)
@@ -239,15 +209,11 @@ class SolvisOptionsFlow(config_entries.OptionsFlow):
             errors=errors,
         )
 
-    async def async_step_features(
-        self, user_input: ConfigType | None = None
-    ) -> FlowResult:
+    async def async_step_features(self, user_input: ConfigType | None = None) -> FlowResult:
         """Handle the feature step."""
         _LOGGER.debug(f"Options flow values_2: {str(self.data)}", DOMAIN)
         if user_input is not None:
             self.data.update(user_input)
             self.hass.config_entries.async_update_entry(self.config, data=self.data)
             return self.async_create_entry(title=self.data[CONF_NAME], data=self.data)
-        return self.async_show_form(
-            step_id="features", data_schema=get_solvis_modules_options(self.data)
-        )
+        return self.async_show_form(step_id="features", data_schema=get_solvis_modules_options(self.data))
