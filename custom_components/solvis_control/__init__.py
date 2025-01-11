3b4fe06f80f97e926ed342c17d3a4f8e1761ed21
Lars Kusch <lars@lars-kusch.de>
2025-01-11 09:53:48 +0100

    Fixes for migration and new issues in pymodbus

--- custom_components/solvis_control/__init__.py
+++ custom_components/solvis_control/__init__.py
@@ -119,8 +119,10 @@ async def async_migrate_entry(hass, config_entry: ConfigEntry):
     if config_entry.version == 1:
 
         new_data = {**config_entry.data}
-        if config_entry.minor_version < 3:
-            _LOGGER.info(f"Migrating from version {config_entry.version}_{config_entry.minor_version}")
+        if current_version == 1 and current_minor_version < 3:
+            _LOGGER.info(
+                f"Migrating from version {current_version}_{current_minor_version}"
+            )
             if CONF_OPTION_1 not in new_data:
                 new_data[CONF_OPTION_1] = False
             if CONF_OPTION_2 not in new_data:
@@ -132,15 +134,19 @@ async def async_migrate_entry(hass, config_entry: ConfigEntry):
             if DEVICE_VERSION not in new_data:
                 new_data[DEVICE_VERSION] = "SC3"
             current_minor_version = 3
-        if config_entry.minor_version < 4:
-            _LOGGER.info(f"Migrating from version {config_entry.version}_{config_entry.minor_version}")
+        if current_version == 1 and current_minor_version < 4:
+            _LOGGER.info(
+                f"Migrating from version {current_version}_{current_minor_version}"
+            )
             if POLL_RATE_DEFAULT not in new_data:
                 new_data[POLL_RATE_DEFAULT] = 30
             if POLL_RATE_SLOW not in new_data:
                 new_data[POLL_RATE_SLOW] = 300
             current_minor_version = 4
-        if config_entry.version < 2 and config_entry.minor_version == 4:
-            _LOGGER.info(f"Migrating from version {config_entry.version}_{config_entry.minor_version}")
+        if current_version == 1 and current_minor_version == 4:
+            _LOGGER.info(
+                f"Migrating from version {current_version}_{current_minor_version}"
+            )
             current_minor_version = 0
             current_version = 2
         hass.config_entries.async_update_entry(
@@ -149,6 +155,8 @@ async def async_migrate_entry(hass, config_entry: ConfigEntry):
             minor_version=current_minor_version,
             version=current_version,
         )
-        _LOGGER.info(f"Migration to version {current_version}_{current_minor_version} successful")
+        _LOGGER.info(
+            f"Migration to version {current_version}_{current_minor_version} successful"
+        )
 
         return True
