3628e0032509cdfdb35eef6a562282492ba2d037
fkhr79 <frank.robens@gmail.com>
2025-04-15 22:54:20 +0200

    Fix: failing tests for sensor.py after changes

--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -70,7 +70,7 @@ async def test_handle_coordinator_update_case2_zero(mock_solvis_sensor):
     with patch("custom_components.solvis_control.sensor._LOGGER.debug") as mock_warning:
         with patch("custom_components.solvis_control.entity.process_coordinator_data", return_value=(True, test_value, {"raw_value": test_value})):
             mock_solvis_sensor._handle_coordinator_update()
-            mock_warning.assert_called_with(f"Division by zero for {mock_solvis_sensor._response_key} with value {test_value}")
+            mock_warning.assert_any_call(f"Division by zero for {mock_solvis_sensor._response_key} with value {test_value}")
     assert mock_solvis_sensor._attr_native_value == test_value
 
 
@@ -95,7 +95,7 @@ async def test_handle_coordinator_update_case3_zero(mock_solvis_sensor):
     with patch("custom_components.solvis_control.sensor._LOGGER.debug") as mock_warning:
         with patch("custom_components.solvis_control.entity.process_coordinator_data", return_value=(True, test_value, {"raw_value": test_value})):
             mock_solvis_sensor._handle_coordinator_update()
-            mock_warning.assert_called_with(f"Division by zero for {mock_solvis_sensor._response_key} with value {test_value}")
+            mock_warning.assert_any_call(f"Division by zero for {mock_solvis_sensor._response_key} with value {test_value}")
     assert mock_solvis_sensor._attr_native_value == test_value
 
 
